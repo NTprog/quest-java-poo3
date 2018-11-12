b1d824cf3a7aec38303f4b9aad2085e0c3b7b00c
Tom Noden <ntprogramation@gmail.com>
2018-11-12 15:39:22 +0100

    quete finnish

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,31 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    public void takeOff() {
+ 	System.out.printf("%s takes off in the sky.%n", this.getName());	
+    }
+
+    public int ascend(int meters) {
+	this.altitude += meters;
+	System.out.printf("%s flies upward " + meters + " meters %n", this.getName());
+    	return meters;
+    }
+
+    public int descend(int meters) {
+    	this.altitude += meters;
+	System.out.printf("%s flies downward " + meters + " meters %n", this.getName());
+	return meters;
+    }
+
+    public void land () {
+
+        if (this.altitude <= 5 ) {
+            System.out.println("Gotta land ma men !");
+            this.altitude = 0;
+        } else {
+            System.out.println("Ain't landin from so high ! I'm too high ... <)~");
+        }
     }
 }
